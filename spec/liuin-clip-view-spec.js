66db501415b106451fd630c424855d167e765f9a
liuin <20893791@qq.com>
2017-01-16 14:33:59 +0800

    0116

--- spec/liuin-clip-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LiuinClipView from '../lib/liuin-clip-view';
-
-describe('LiuinClipView', () => {
-  it('has one valid test', () => {
-    // expect('life').toBe('easy');
-  });
-});
